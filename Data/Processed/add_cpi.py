277d2e6bf661bc2c2bc9be6a7b8e7f0b3852241a
aladlyy <muhammad.adly@mail.utoronto.ca>
2026-02-28 16:17:54 -0500

    Add files via upload

--- Data/Processed/add_cpi.py
+++ Data/Processed/add_cpi.py
@@ -13,11 +13,11 @@ cpi = pd.read_excel("CPI US.xlsx", sheet_name="Monthly")
 # # Create add_cpi Function
 # This function is used to convert the monthly CPI into quarterly, then merge it to the Airline Ticket Dataset after adjusting the CPI so that Q1 2022 becomes the base year with CPI = 100.
 
-# In[2]:
+# In[9]:
 
 
-def add_cpi(df, cpi):
-    df["fare_per_miles"] = df["fare"]/df["nsmiles"]
+def add_cpi(airline_ticket: pd.DataFrame, cpi:pd.DataFrame):
+    airline_ticket["fare_per_miles"] = airline_ticket["fare"]/df["nsmiles"]
 
     cpi["Year"] = cpi["observation_date"].dt.year
     cpi["month"] = cpi["observation_date"].dt.month
@@ -33,16 +33,16 @@ def add_cpi(df, cpi):
     cpi_q["cpi_adj"] = cpi_q["cpi_q"]/284.905667 * 100
     cpi_q.drop([14, 15, 16], axis=0, inplace=True)
 
-    df = df.merge(cpi_q[["Year", "quarter", "cpi_adj"]], on=["Year", "quarter"], how="right")
+    airline_ticket = airline_ticket.merge(cpi_q[["Year", "quarter", "cpi_adj"]], on=["Year", "quarter"], how="right")
     nom_price = ["fare", "fare_lg", "fare_low"]
 
     for x in nom_price:
-        df[x + "_real"] = df[x] * (100 / df["cpi_adj"])
+        airline_ticket[x + "_real"] = airline_ticket[x] * (100 / airline_ticket["cpi_adj"])
 
-    return df
+    return airline_ticket
 
 
-# In[3]:
+# In[10]:
 
 
 add_cpi(df, cpi)
